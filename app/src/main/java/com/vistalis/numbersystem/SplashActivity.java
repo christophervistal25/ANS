d64fb5ca35757bd663a9e7200edd55b4f35cd155
christophervistal25 <christophervistal25@gmail.com>
2019-05-22 13:56:40 +0800

    Add assets and add design for Splash Screen

--- app/src/main/java/com/vistalis/numbersystem/SplashActivity.java
+++ app/src/main/java/com/vistalis/numbersystem/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.vistalis.numbersystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Button btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(view -> {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
